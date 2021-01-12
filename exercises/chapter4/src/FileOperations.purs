548fc73738a0d5a86ff9e8124ac4386e6e1ac50a
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2021-01-12 08:05:24 +0100

    Complete chapter4 exercises

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -2,13 +2,43 @@ module FileOperations where
 
 import Prelude
 
-import Data.Path (Path, ls)
-import Data.Array (concatMap, (:))
+import Data.Path (Path, ls, isDirectory, size, filename, root)
+import Data.Array (concatMap, (:), filter, head)
+import Data.Foldable (foldl)
+import Data.Maybe (Maybe(), fromMaybe)
 
 allFiles :: Path -> Array Path
-allFiles root = root : concatMap allFiles (ls root)
+allFiles path = path : concatMap allFiles (ls path)
 
 allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+onlyFiles :: Path -> Array Path
+onlyFiles = filter (not <<< isDirectory) <<< allFiles'
+
+maxInt :: Int
+maxInt = 2147483647
+
+findLargestAndSmallest :: Path -> { largest :: Path, smallest :: Path }
+findLargestAndSmallest rootPath =
+  foldl updateExtrema { largest: rootPath, smallest: rootPath } $ onlyFiles rootPath
+  where
+    updateExtrema ({ largest, smallest }) path =
+      { largest: if (isLarger largest path) then largest else path
+      , smallest: if (isSmaller smallest path) then smallest else path
+      }
+    compare default f a b = f (fromMaybe default (size a)) (fromMaybe default (size b))
+    isLarger a b = compare 0 (>) a b
+    isSmaller a b = compare maxInt (<) a b
+
+
+whereIs :: String -> Maybe Path
+whereIs pathName = head $ whereIs' root pathName
+  where
+    whereIs' path name = do
+      file <- ls path
+      if filename file == name
+        then pure path
+        else whereIs' file name
--- exercises/chapter4/test/Main.purs
+++ exercises/chapter4/test/Main.purs
@@ -2,11 +2,102 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Effect (Effect)
+import Effect.Console (logShow)
 import Data.Path (root)
-import Data.Foldable (for_)
+import Data.Foldable (for_, foldl)
 import FileOperations (allFiles)
 
-main :: Eff (console :: CONSOLE) Unit
+import Data.Array (null, filter, (..), length, (:))
+import Data.Array.Partial (head, tail)
+import Data.Tuple (Tuple(..))
+import Partial.Unsafe (unsafePartial)
+import Control.MonadZero (guard)
+
+main :: Effect Unit
 main = for_ (allFiles root) logShow
+
+isEven :: Int -> Boolean
+isEven 0 = true
+isEven a = isOdd (a - 1)
+
+isOdd :: Int -> Boolean
+isOdd 0 = false
+isOdd a = isEven (a - 1)
+
+isEven2 :: Int -> Boolean
+isEven2 0 = true
+isEven2 1 = false
+isEven2 a = isEven2 (a - 2)
+
+countEven :: Array Int -> Int
+countEven = countEven' 0
+  where
+    countEven' acc arr =
+      if null arr
+        then acc
+        else countEven' updatedCount arrTail
+      where
+      updatedCount = if (isEven2 $ unsafePartial head arr)
+        then acc + 1
+        else acc
+      arrTail = unsafePartial tail arr
+
+squares :: Array Int -> Array Int
+squares = map (\a -> a * a)
+
+positives :: Array Int -> Array Int
+positives = (<$?>) $ (\a -> a > 0)
+
+infixr 8 filter as <$?>
+
+factors :: Int -> Array (Array Int)
+factors n = do
+  i <- 1 .. n
+  j <- i .. n
+  guard $ i * j == n
+  pure [i, j]
+
+isPrime :: Int -> Boolean
+isPrime n = length (factors n) == 1
+
+cartesianProduct :: forall a b. Array a -> Array b -> Array (Tuple a b)
+cartesianProduct arrA arrB = do
+  a <- arrA
+  b <- arrB
+  pure $ Tuple a b
+
+triples :: Int -> Array (Array Int)
+triples n = do
+  a <- 1 .. n
+  b <- 1 .. n
+  c <- 1 .. n
+  guard $ a * a + b * b == c * c
+  pure [a, b, c]
+
+-- How to get rid of the duplicates?
+factorizations :: Int -> Array (Array Int)
+factorizations n = factorizations' n []
+  where
+    factorizations' n acc = do
+      i <- 1 .. (div n 2)
+      guard $ (mod n i) == 0
+      if i == 1
+        then pure (acc <> [n])
+        else factorizations' (div n i) (acc <> [i])
+
+allTrue :: Array Boolean -> Boolean
+allTrue = foldl (&&) true
+
+count :: forall a. (a -> Boolean) -> Array a -> Int
+count = count' 0
+  where
+    count' acc _ [] = acc
+    count' acc p xs =
+      if p (unsafePartial head xs)
+        then count' (acc + 1) p (unsafePartial tail xs)
+        else count' acc p (unsafePartial tail xs)
+
+
+reverseFoldl :: Array Int -> Array Int
+reverseFoldl = foldl (\as a -> a : as) []
