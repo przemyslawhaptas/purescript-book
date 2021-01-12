24b51e718341fe5500ee17aa272d428d2be97589
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2021-01-12 08:07:11 +0100

    Complete chapter5 exercises

--- exercises/chapter5/src/Data/Picture.purs
+++ exercises/chapter5/src/Data/Picture.purs
@@ -20,6 +20,9 @@ data Shape
   | Rectangle Point Number Number
   | Line Point Point
   | Text Point String
+  | Clipped Picture
+
+type Picture = Array Shape
 
 showShape :: Shape -> String
 showShape (Circle c r) =
@@ -30,8 +33,8 @@ showShape (Line start end) =
   "Line [start: " <> showPoint start <> ", end: " <> showPoint end <> "]"
 showShape (Text loc text) =
   "Text [location: " <> showPoint loc <> ", text: " <> show text <> "]"
-
-type Picture = Array Shape
+showShape (Clipped picture) =
+  foldl (<>) "" (map showShape picture)
 
 showPicture :: Picture -> Array String
 showPicture = map showShape
@@ -76,6 +79,8 @@ shapeBounds (Text (Point { x, y }) _) = Bounds
   , bottom: y
   , right:  x
   }
+shapeBounds (Clipped picture) = bounds picture
+
 
 union :: Bounds -> Bounds -> Bounds
 union (Bounds b1) (Bounds b2) = Bounds
--- exercises/chapter5/test/Main.purs
+++ exercises/chapter5/test/Main.purs
@@ -2,9 +2,10 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Picture (Point(..), Shape(..), Picture, bounds, showBounds)
+import Effect (Effect)
+import Effect.Console (log)
+import Data.Picture (Point(..), Shape(..), Picture, Bounds(..), bounds, showBounds, shapeBounds)
+import Data.Maybe (Maybe(..))
 
 circle :: Shape
 circle = Circle (Point { x: 0.0, y: 0.0 }) 10.0
@@ -15,5 +16,50 @@ rectangle = Rectangle (Point { x: 10.0, y: 10.0 }) 10.0 10.0
 picture :: Picture
 picture = [circle, rectangle]
 
-main :: Eff (console :: CONSOLE) Unit
+main :: Effect Unit
 main = log (showBounds (bounds picture))
+
+fact :: Int -> Int
+fact 0 = 1
+fact n = fact (n - 1) * n
+
+factTail :: Int -> Int
+factTail n = factTail' n 1
+  where
+    factTail' 0 acc = acc
+    factTail' n' acc = factTail' (n' - 1) (acc * n')
+
+binomialCoefficient :: Int -> Int -> Int
+binomialCoefficient n k
+  | n < k = 0
+  | otherwise = (fact n) / ((fact k) * fact (n - k))
+
+type Address = { street :: String, city :: String }
+type Person = { name :: String, address :: Address }
+
+sameCity :: Person -> Person -> Boolean
+-- sameCity ::
+--   forall r1 r2 r3 r4 eq. Eq eq =>
+--     { address :: { city :: eq | r1 } | r2 } ->
+--     { address :: { city :: eq | r3 } | r4 } ->
+--     Boolean
+sameCity { address: { city: cityA } } { address: { city: cityB } } = cityA == cityB
+
+fromSingleton :: forall a. a -> Array a -> a
+fromSingleton _ [a] = a
+fromSingleton default _ = default
+
+double :: Shape -> Shape
+double (Circle origin r) = Circle origin (r * 2.0)
+double (Rectangle origin a b) = Rectangle origin (a * 2.0) (b * 2.0)
+double (Line start (Point { x, y })) = Line start (Point { x: x * 2.0, y: y * 2.0 })
+double text = text
+
+shapeText :: Shape -> Maybe String
+shapeText (Text _ text) = Just text
+shapeText _ = Nothing
+
+shapeArea :: Shape -> Number
+shapeArea shape = boundsArea (shapeBounds shape)
+  where
+    boundsArea (Bounds { top, left, bottom, right }) = (right - left) * (top - bottom)
