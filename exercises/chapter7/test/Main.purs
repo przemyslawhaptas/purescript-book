12542f34e2ebe21748af511d1578e0ddefeffb7e
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2021-03-02 08:40:49 +0100

    Do exercise 7.11/1

--- exercises/chapter7/test/Main.purs
+++ exercises/chapter7/test/Main.purs
@@ -6,6 +6,8 @@ import Effect (Effect)
 import Effect.Console (logShow)
 import Data.Maybe (Maybe(..))
 import Control.Apply (lift2)
+import Data.Traversable (class Traversable, traverse, sequenceDefault)
+import Data.Foldable (class Foldable, foldMap, foldrDefault, foldlDefault)
 
 import Data.AddressBook (examplePerson)
 import Data.AddressBook.Validation (validatePerson)
@@ -36,3 +38,21 @@ infixl 6 maybeDiv as /?
 combineMaybe :: forall a f. Applicative f => Maybe (f a) -> f (Maybe a)
 combineMaybe Nothing = pure Nothing
 combineMaybe (Just a) = Just <$> a
+
+
+data Tree a = Leaf | Branch (Tree a) a (Tree a)
+
+instance functorTree :: Functor Tree where
+  map _ Leaf = Leaf
+  map f (Branch left val right) = Branch (map f left) (f val) (map f right)
+
+instance foldableTree :: Foldable Tree where
+  foldMap f Leaf = mempty
+  foldMap f (Branch left val right) = (foldMap f left) <> f val <> (foldMap f right)
+  foldr f = foldrDefault f
+  foldl f = foldlDefault f
+
+instance traversableTree :: Traversable Tree where
+  traverse _ Leaf = pure Leaf
+  traverse f (Branch left val right) = Branch <$> (traverse f left) <*> (f val) <*> (traverse f right) -- in-order
+  sequence = sequenceDefault
