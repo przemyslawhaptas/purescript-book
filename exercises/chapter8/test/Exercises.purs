387fe72f225dd626540d74589712d2b52be5aa9d
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2021-04-18 21:46:17 +0200

    Do exercises 8.17

--- exercises/chapter8/test/Exercises.purs
+++ exercises/chapter8/test/Exercises.purs
@@ -3,11 +3,16 @@ module Test.Exercises where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log, logShow)
+import Effect.Console (logShow)
+import Effect.Exception (throwException, error)
+import Effect.Random (randomInt)
 import Data.Maybe (Maybe)
-import Data.Array ((:), head, tail, sort, nubEq, foldM)
+import Data.Array (head, tail, sort, nubEq, foldM)
 import Data.List (List(..))
-import Control.MonadPlus (guard)
+import Data.Int (toNumber)
+import Control.Monad.ST.Ref as STRef
+import Control.Monad.ST as ST
+import Math (sqrt)
 
 type AppProps = {}
 
@@ -32,3 +37,27 @@ filterM f (Cons x xs) = do
       pure (Cons x) <*> (filterM f xs)
     else
       filterM f xs
+
+unsafeDivide :: Int -> Int -> Effect Int
+unsafeDivide _ 0 = throwException (error "You cannot divide by 0!")
+unsafeDivide a b = pure (a / b)
+
+inCircle :: Int -> Int -> Int -> Boolean
+inCircle r x y = distance <= toNumber r where
+  distance = sqrt $ toNumber $ ((r - x) * (r - x)) + ((r - y) * (r - y))
+
+estimatePi :: Effect Number
+estimatePi = do
+  a <- randomInt 1000 10000
+  let
+    n = a * a
+    r = a / 2
+    countInCircle = ST.run do
+      total <- STRef.new 0
+      ST.for 0 n \i -> do
+        let
+          y = i / a
+          x = i - y * a
+        STRef.modify (if (inCircle r x y) then ((+) 1) else identity) total
+      STRef.read total
+  pure $ (toNumber (4 * countInCircle)) / (toNumber n)
