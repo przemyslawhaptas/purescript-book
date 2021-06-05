dfe5e2ed22102d92477993ee19238f6a8f375d49
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2021-06-05 12:49:27 +0200

    Complete exercises 11.6.1-2

--- exercises/chapter11/test/Exercises.purs
+++ exercises/chapter11/test/Exercises.purs
@@ -5,14 +5,17 @@ import Prelude
 import Control.Monad.Reader (Reader, ask, local, runReader)
 import Control.Monad.State (State, execState)
 import Control.Monad.State.Class (modify)
+import Control.Monad.Writer (Writer, execWriter, tell)
 import Data.Foldable (traverse_, fold)
 import Data.List.Lazy (replicate)
 import Data.Maybe (Maybe(..))
+import Data.Monoid.Additive (Additive(..))
 import Data.String (joinWith)
 import Data.String.CodeUnits (toCharArray)
 import Data.Traversable (sequence)
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Newtype (unwrap)
 
 sumArray :: Array Number -> State Number Unit
 sumArray = traverse_ \n -> modify \sum -> sum + n
@@ -25,6 +28,9 @@ main = log $ render $ cat
       , line "So am I"
       , indent $ line "I am even more indented"
       ]
+  , line "#sumArrayW"
+  , indent $ line "in: [1.0, 2.0, 3.0, 4.0, 5.0]"
+  , indent $ line $ "out " <> show (unwrap $ execWriter $ sumArrayW [1.0, 2.0, 3.0, 4.0, 5.0])
   ]
 
 testParens :: String -> Boolean
@@ -57,3 +63,18 @@ cat docs = do
 
 render :: Doc -> String
 render doc = runReader doc 0
+
+sumArrayW :: Array Number -> Writer (Additive Number) Unit
+sumArrayW = traverse_ \n -> tell (Additive n)
+
+collatz :: Int -> Int
+collatz = collatz_ 0 where
+  collatz_ count 1 = count
+  collatz_ count n = collatz_ (count + 1) (if n `mod` 2 == 0 then (n / 2) else (3 * n + 1))
+
+collatzW :: Int -> Writer (Array String) Int
+collatzW = collatz_ 0 where
+  collatz_ count 1 = pure count
+  collatz_ count n = do
+    tell ["collatz_ " <> show count <> " " <> show n]
+    collatz_ (count + 1) (if n `mod` 2 == 0 then (n / 2) else (3 * n + 1))
