3eea2d3ace166cbb32469b1143dbe3681cf3d2dd
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2021-06-05 11:36:23 +0200

    Complete exercises 11.5.1-4

--- exercises/chapter11/test/Exercises.purs
+++ exercises/chapter11/test/Exercises.purs
@@ -2,19 +2,30 @@ module Test.Exercises where
 
 import Prelude
 
+import Control.Monad.Reader (Reader, ask, local, runReader)
 import Control.Monad.State (State, execState)
 import Control.Monad.State.Class (modify)
-import Data.Foldable (traverse_)
-import Data.String.CodeUnits (toCharArray)
-import Effect (Effect)
-import Effect.Console (logShow)
+import Data.Foldable (traverse_, fold)
+import Data.List.Lazy (replicate)
 import Data.Maybe (Maybe(..))
+import Data.String (joinWith)
+import Data.String.CodeUnits (toCharArray)
+import Data.Traversable (sequence)
+import Effect (Effect)
+import Effect.Console (log)
 
 sumArray :: Array Number -> State Number Unit
 sumArray = traverse_ \n -> modify \sum -> sum + n
 
 main :: Effect Unit
-main = logShow "Exercises"
+main = log $ render $ cat
+  [ line "Here is some indented text:"
+  , indent $ cat
+      [ line "I am indented"
+      , line "So am I"
+      , indent $ line "I am even more indented"
+      ]
+  ]
 
 testParens :: String -> Boolean
 testParens s = isBalanced $ execState
@@ -27,3 +38,22 @@ testParens s = isBalanced $ execState
     countParen char (Just count) = if newCount < 0 then Nothing else (Just newCount)
       where
       newCount = if char == '(' then count + 1 else count - 1
+
+type Level = Int
+type Doc = Reader Level String
+
+line :: String -> Doc
+line str = do
+  level <- ask
+  pure $ (fold (replicate level " ")) <> str
+
+indent :: Doc -> Doc
+indent = local ((+) 1)
+
+cat :: Array Doc -> Doc
+cat docs = do
+  strings <- sequence docs
+  pure $ joinWith "\n" strings
+
+render :: Doc -> String
+render doc = runReader doc 0
