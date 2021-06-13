8f4a4fcd0f3897ff95052e2b01a6d487ac436990
Przemysław Haptaś <przemyslawhaptas@gmail.com>
2021-06-13 09:54:58 +0200

    Complete exercises 12.6.1-3

--- exercises/chapter12/src/Network/HTTP/Client.purs
+++ exercises/chapter12/src/Network/HTTP/Client.purs
@@ -2,10 +2,12 @@ module Network.HTTP.Client where
 
 import Prelude
 
-import Control.Monad.Cont.Trans (ContT(..))
-import Effect (Effect)
+import Control.Monad.Cont.Trans (ContT(..), runContT)
+import Control.Monad.Except.Trans (ExceptT(..), runExceptT)
 import Data.Either (Either(..))
 import Data.Function.Uncurried (Fn3, runFn3)
+import Effect (Effect)
+import Files (writeFileContEx, FilePath)
 import Types (Async)
 
 type URI = String
@@ -17,3 +19,14 @@ foreign import getImpl :: Fn3 URI
 
 get :: URI -> Async (Either String String)
 get req = ContT $ \k -> runFn3 getImpl req (k <<< Right) (k <<< Left)
+
+getEx :: URI -> ExceptT String Async String
+getEx req = ExceptT $ get req
+
+downloadEx :: URI -> FilePath -> ExceptT String Async Unit
+downloadEx req dest = do
+  response <- getEx req
+  writeFileContEx dest response
+
+download :: URI -> FilePath -> Effect Unit
+download req dest = runContT (runExceptT $ downloadEx req dest) mempty
